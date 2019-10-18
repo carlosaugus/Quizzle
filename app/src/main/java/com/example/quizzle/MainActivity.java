a94a3462b9eccb89afe131ceeb1b833023b87b37
Carlos Nascimento <carlos.augus@outlook.com>
2019-10-18 19:06:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/quizzle/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.quizzle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void abrirLogin(View view){
+        Intent it = new Intent(this, LoginActivity.class);
+        startActivity(it);
+    }
+
+    //public void abrirConvidado(View view){
+    //    Intent it = new Intent(this, ConvidadoActivity.class);
+    //    startActivity(it);
+    //}
+
+    public void abrirCadastro(View view){
+        Intent it = new Intent(this, CadastroActivity.class);
+        startActivity(it);
+    }
+}
