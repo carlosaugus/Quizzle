b64ad0274c94d24b5356dd105617f2f0f983d1b3
adams <adamsrogerio@hotmail.com>
2019-10-21 19:26:33 -0300

    Adicionados fundos de telas e dos botões desenvolvidos pelo Matheus

--- app/src/main/java/com/example/quizzle/MainActivity.java
+++ app/src/main/java/com/example/quizzle/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    //teste
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
