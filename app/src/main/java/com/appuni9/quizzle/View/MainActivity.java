6c01b249c2de44c4f52f329dc743fef0abe5a27a
thiagomarcondes9 <thiago_marcondes@outlook.com.br>
2019-11-09 20:40:04 -0300

    Criação do fundo musical e o botão mudo.

--- app/src/main/java/com/appuni9/quizzle/View/MainActivity.java
+++ app/src/main/java/com/appuni9/quizzle/View/MainActivity.java
@@ -3,6 +3,7 @@ package com.appuni9.quizzle.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -11,12 +12,34 @@ import com.appuni9.quizzle.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    MediaPlayer mp = new MediaPlayer();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        mp.stop();
+        mp = MediaPlayer.create(this, R.raw.djmarky);
+        mp.start();
+        mp.setLooping(true);
     }
+
+    public void btnMudo(View view){
+        mp.stop();
+    }
+
+//    public void onStop() {
+//
+//        super.onStop();
+//
+//        if (mp.isPlaying()) {
+//            mp.stop();
+//        }
+//        mp.release();
+//    }
 
     public void abrirLogin(View view){
         Intent it = new Intent(this, LoginActivity.class);
